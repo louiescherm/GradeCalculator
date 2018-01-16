709832bb3fde91b8a7950b9c98466a6d235b732f
= <=>
2018-01-16 11:28:04 -0800

    Initial commit

---
+++ Index.js
@@ -0,0 +1,23 @@
+
+function calculateCurrentGrade() {
+
+}
+
+function convertArrayStringToNumber(str){
+    var arr = str.split(arr);
+    return arr;
+}
+
+function averageArray(arr){
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++){
+        sum +=
+    }
+}
+
+function calculateCurrentGrade(){
+
+}
+function calculateGradeNeeded(){
+
+}
